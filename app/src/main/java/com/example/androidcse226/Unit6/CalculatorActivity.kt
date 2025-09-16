cb35aac20beb3eac294d4279ee41e133f410e533
Ishaant Kumar Singh <ishaant97@gmail.com>
2025-09-16 20:34:19 +0530

    Unit 6 Done

---
+++ app/src/main/java/com/example/androidcse226/Unit6/CalculatorActivity.kt
@@ -0,0 +1,28 @@
+package com.example.androidcse226.Unit6
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidcse226.R
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_calculator)
+
+        val num1 = findViewById<EditText>(R.id.num1)
+        val num2 = findViewById<EditText>(R.id.num2)
+        val addBtn = findViewById<Button>(R.id.addBtn)
+        val result = findViewById<TextView>(R.id.result)
+
+        addBtn.setOnClickListener {
+            val a = num1.text.toString().toIntOrNull() ?: 0
+            val b = num2.text.toString().toIntOrNull() ?: 0
+            result.text = (a + b).toString()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidcse226/Unit6/TestingActivity.kt
@@ -0,0 +1,24 @@
+package com.example.androidcse226.Unit6
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidcse226.R
+
+class TestingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_testing)
+
+        val calc = Calculator()
+
+        val sum = calc.add(5, 3)
+        Log.d("MainActivity", "Sum = $sum")
+
+        val div = calc.divide(10, 0) // will log error
+        Log.d("MainActivity", "Division result = $div")
+    }
+}
+
